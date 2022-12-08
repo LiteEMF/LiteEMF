49d3ae1ec51f8072d6aa0db2526975e8c27b72eb
LiteEMF <cesar10260@gmail.com>
2022-12-08 22:15:54 +0800

    add template

---
+++ template.h
@@ -0,0 +1,48 @@
+/* 
+*   BSD 2-Clause License
+*   Copyright (c) 2022, LiteEMF
+*   All rights reserved.
+*   This software component is licensed by LiteEMF under BSD 2-Clause license,
+*   the "License"; You may not use this file except in compliance with the
+*   License. You may obtain a copy of the License at:
+*       opensource.org/licenses/BSD-2-Clause
+* 
+*/
+
+
+#ifndef _template_h
+#define _template_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/******************************************************************************************************
+** Defined
+*******************************************************************************************************/
+
+
+
+/******************************************************************************************************
+**	Parameters
+*******************************************************************************************************/
+
+
+
+/*****************************************************************************************************
+**  Function
+******************************************************************************************************/
+void template_init(void);
+void template_deinit(void);
+void template_handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
+
+
+
